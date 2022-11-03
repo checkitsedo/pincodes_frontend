af37ebe496acdeb939feaf6299e46e0c6fddb32a
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2022-11-03 14:22:57 +0100

    Update ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,8 +1,10 @@
 #
-# Table structure for table 'tx_formpincodes_domain_model_pincodes'
+# Table structure for table 'tx_pincodesfrontend_domain_model_pincode'
 #
-CREATE TABLE tx_formpincodes_domain_model_pincodes (
-
+CREATE TABLE tx_pincodesfrontend_domain_model_pincode (
+    
+    pincode varchar(25) DEFAULT '' NOT NULL,
+    active tinyint(4) unsigned DEFAULT 0 NOT NULL,
     pincodes_user int(11) DEFAULT '0' NOT NULL,
 
 );
