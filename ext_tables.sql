2d3063aff73cbf7538c9d7213b59454070addd7b
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2022-11-03 13:27:31 +0100

    Create ext_tables.sql

---
+++ ext_tables.sql
@@ -0,0 +1,8 @@
+#
+# Table structure for table 'tx_formpincodes_domain_model_pincodes'
+#
+CREATE TABLE tx_formpincodes_domain_model_pincodes (
+
+    pincodes_user int(11) DEFAULT '0' NOT NULL,
+
+);
